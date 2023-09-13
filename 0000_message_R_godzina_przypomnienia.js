6ec4b9686231aafc9ba84e6ec30e67abcff9d584
wojtish <20014605+wojtish@users.noreply.github.com>
2023-09-13 11:29:52 +0200

    Update 0000_message_R_godzina_przypomnienia.js

--- 0000_message_R_godzina_przypomnienia.js
+++ 0000_message_R_godzina_przypomnienia.js
@@ -1,7 +1,7 @@
 moment.locale('pl');
 //toLocaleDateString()
 var K = moment(R).format('LLLL');
-var T = moment(R).format('hH:mm:ss');
+var T = moment(R).format('HH:mm:ss');
 entry().set("godzina przypomnienia", T)
 
 message(K)
