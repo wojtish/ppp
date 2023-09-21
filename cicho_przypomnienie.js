89664f196acdc44086640d84b414deb476bc3194
wojtish <20014605+wojtish@users.noreply.github.com>
2023-09-21 13:57:02 +0200

    Update cicho_przypomnienie.js

--- cicho_przypomnienie.js
+++ cicho_przypomnienie.js
@@ -1,7 +1,11 @@
 // cicho_przypomnienie.js
 if (entry().field("zong") == "cicho"){
-entry().set("zong","Sprawdź to przypomnienie")}
+entry().set("zong","Sprawdź to przypomnienie");
+entry().set("zongob","sp.rawdź");
+}
 //message(entry().field("mniejwiecej"))}
 else  {
-entry().set("zong","cicho")}
+entry().set("zong","cicho");
+entry().set("zongob","ci.cho");
+}
 message(entry().field("zong"))
