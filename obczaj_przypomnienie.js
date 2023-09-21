0c277fb63b8bd89341185797f5b61a503c665e5c
wojtish <20014605+wojtish@users.noreply.github.com>
2023-09-21 13:53:07 +0200

    Update obczaj_przypomnienie.js

--- obczaj_przypomnienie.js
+++ obczaj_przypomnienie.js
@@ -1,7 +1,11 @@
 // obczaj_przypomnienie.js
 if (entry().field("zong") == "cicho"){
-entry().set("zong","Obczaj to przypomnienie")}
+entry().set("zong","Obczaj to przypomnienie")
+entry().set("zongob","ob.czaj")
+}
 //message(entry().field("mniejwiecej"))}
 else  {
-entry().set("zong","cicho")}
+entry().set("zong","cicho")
+entry().set("zongob","ci.cho")
+}
 message(entry().field("zong"))
